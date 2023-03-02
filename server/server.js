a8b9509fa5aef9acc77e9402d6e74bbf14a26188
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-03-02 04:24:47 -0800

    insert affiliate stats data

--- server/server.js
+++ server/server.js
@@ -17,12 +17,14 @@ import Product from './models/Product.js';
 import ProductStat from './models/ProductStat.js';
 import Transaction from './models/Transaction.js';
 import OverallStat from './models/OverallStat.js';
+import AffiliateStat from './models/AffiliateStat.js';
 import { 
     dataUser, 
     dataProduct, 
     dataProductStat, 
     dataTransaction, 
-    dataOverallStat 
+    dataOverallStat,
+    dataAffiliateStat
 } from './data/index.js';
 
 // Configuration
@@ -55,6 +57,7 @@ mongoose.connection
         // ProductStat.insertMany(dataProductStat);
         // Transaction.insertMany(dataTransaction);
         // OverallStat.insertMany(dataOverallStat);
+        // AffiliateStat.insertMany(dataAffiliateStat);
     })
     .on("close", () => console.log("Disconnected from MongoDB"))
     .on("error", () => console.log(err.message));
