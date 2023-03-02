7735f125289ee75c491aad5cc8faa7b03c1a8600
jeremiahtaylor2021 <jeremiahtaylor2021@gmail.com>
2023-03-02 04:00:44 -0800

    fixed a missing await

--- server/controllers/management.js
+++ server/controllers/management.js
@@ -4,9 +4,9 @@ import User from "../models/User.js";
 
 export const getAdmins = async (req, res) => {
     try {
-        const admins = User.find({ role: "admin" }).select("-password");
+        const admins = await User.find({ role: "admin" }).select("-password");
         res.status(200).json(admins);
     } catch (error) {
-        res.status(404).json({message: error.message });
+        res.status(404).json({ message: error.message });
     }
 }
